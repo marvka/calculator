44454528ae6dbab02363daebbff2276b99d64fad
marvkal <marvka@protonmail.com>
2020-09-22 16:22:55 +0100

    First draft of operate function

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case "+":
+      return add(num1, num2);
+      break;
+    case "-":
+      return subtract(num1, num2);
+      break;
+    case "*":
+      return multiply(num1, num2);
+      break;
+    case "/":
+      return divide(num1, num2);
+      break;
+    default:
+      return "Error";
+  }
+}
