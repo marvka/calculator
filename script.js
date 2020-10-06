805aa3618c1e9fc2c63058fc437297a1fa796a07
marvkal <marvka+github@protonmail.com>
2020-10-06 06:03:58 +0100

    Implement basic functionality for operator buttons (incl. helper function)

--- script.js
+++ script.js
@@ -12,30 +12,37 @@ function divide(num1, num2) {
   return num1 / num2;
 }
 function operate(operator, num1, num2) {
+  num1 = +num1;
+  num2 = +num2;
   switch (operator) {
-    case "+":
+    case '+':
       return add(num1, num2);
       break;
-    case "-":
+    case '-':
       return subtract(num1, num2);
       break;
-    case "*":
+    case '*':
       return multiply(num1, num2);
       break;
-    case "/":
+    case '/':
       return divide(num1, num2);
       break;
     default:
-      return "Error";
+      return 'Error';
   }
 }
 
 // Function to check type of value
 function isOperator(value) {
-  return /[-+%*]/.test(value);
+  return /[-+/*]/.test(value);
 }
 function isNumber(value) {
   return /[0-9]/.test(value);
+}
+
+// Helper functions
+function operatorIndex() {
+  return display.textContent.length - 2;
 }
 
 // Button functionality
@@ -46,42 +53,69 @@ function numButton(event) {
 }
 function equalsButton(event) {}
 function clearButton(event) {
-  num1 = "";
-  num2 = "";
-  numBuffer = "";
-  result = "";
-  operator = "";
-  display.textContent = "";
+  num1 = '';
+  num2 = '';
+  numBuffer = '';
+  operator = '';
+  display.textContent = '';
 }
-function operatorButton(event) {}
+function operatorButton(event) {
+  // Allow negative number in numBuffer
+  if (!numBuffer && !display.textContent && event.target.textContent == '-') {
+    numBuffer += '-';
+    display.textContent += '-';
+  }
+  // Change operator if already exists
+  if (numBuffer == '' && isOperator(display.textContent.charAt(operatorIndex()))
+  ) {
+    let output = display.textContent.split('');
+    output[operatorIndex()] = event.target.textContent;
+    display.textContent = output.join('');
+    operator = event.target.textContent;
+  } else if (!numBuffer) {
+      return;
+  } // If there's no operator yet and only the first number entered by user
+    else if (numBuffer && !num1 && !operator) {
+      num1 = numBuffer;
+      numBuffer = '';
+      operator = event.target.textContent;
+      display.textContent += ` ${operator} `;
+  } else if (numBuffer && num1 !== '' && !num2) {
+      num2 = numBuffer;
+      num1 = operate(operator, num1, num2);
+      numBuffer = '';
+      num2 = '';
+      operator = event.target.textContent;
+      display.textContent = `${num1} ${operator} `;
+  }
+}
 function dotButton(event) {
-  if (numBuffer.includes(".")) {
+  if (numBuffer.includes('.')) {
     return;
   } else {
-    numBuffer += ".";
-    display.textContent += ".";
+    numBuffer += '.';
+    display.textContent += '.';
   }
 }
 
 // Main code
 let num1,
   num2,
-  numBuffer = "",
-  result,
+  numBuffer = '',
   operator;
-let buttons = document.querySelectorAll("button");
-let display = document.querySelector("#display");
+let buttons = document.querySelectorAll('button');
+let display = document.querySelector('#display');
 
 buttons.forEach((button) => {
   if (isNumber(button.textContent)) {
-    button.addEventListener("click", numButton);
-  } else if (button.textContent === "=") {
-    button.addEventListener("click", equalsButton);
-  } else if (button.textContent === "Clear") {
-    button.addEventListener("click", clearButton);
+    button.addEventListener('click', numButton);
+  } else if (button.textContent === '=') {
+    button.addEventListener('click', equalsButton);
+  } else if (button.textContent === 'Clear') {
+    button.addEventListener('click', clearButton);
   } else if (isOperator(button.textContent)) {
-    button.addEventListener("click", operatorButton);
-  } else if (button.textContent === ".") {
-    button.addEventListener("click", dotButton);
+    button.addEventListener('click', operatorButton);
+  } else if (button.textContent === '.') {
+    button.addEventListener('click', dotButton);
   }
 });
