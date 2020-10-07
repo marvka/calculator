c62d3a13658e274927ba543862150d62f6e9d555
marvkal <marvka+github@protonmail.com>
2020-10-07 13:49:58 +0100

    Refactor operate function, implement operator buttons

--- script.js
+++ script.js
@@ -11,9 +11,11 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
   return num1 / num2;
 }
-function operate(operator, num1, num2) {
-  num1 = +num1;
-  num2 = +num2;
+function operate() {
+  let num1 = +display.textContent.match(/^-?\d+/);
+  let num2 = +display.textContent.match(/\d+$/);
+  let operator = display.textContent.match(/(?:^-?\d+ )([-+/*])(?: \d+)/)[1];
+
   switch (operator) {
     case '+':
       return add(num1, num2);
@@ -32,14 +34,6 @@ function operate(operator, num1, num2) {
   }
 }
 
-// Function to check type of value
-function isOperator(value) {
-  return /[-+/*]/.test(value);
-}
-function isNumber(value) {
-  return /[0-9]/.test(value);
-}
-
 // Helper functions
 function operatorIndex() {
   return display.textContent.length - 2;
@@ -53,16 +47,28 @@ function addNumber(event) {
   display.textContent += event.target.textContent;
 }
 function equals() {
-  if (/\d+ [-+/*] $/.test(display.textContent)) {
+  if (/^-?\d+ [-+/*] $/.test(display.textContent)) {
     display.textContent = display.textContent.substring(
       0,
       display.textContent.length - 3
     );
-  } else if (/\d+ [-+\/*] \d+/.test(display.textContent)) {
-    let num1 = display.textContent.match(/^\d+/);
-    let num2 = display.textContent.match(/\d+$/);
-    let operator = display.textContent.match(/(?:\d+ )([-+/*])(?: \d+)/)[1];
-    display.textContent = operate(operator, num1, num2);
+  } else if (/^-?\d+ [-+/*] \d+$/.test(display.textContent)) {
+    display.textContent = operate();
+  }
+}
+function addOperator(event) {
+  // Can't have two operators following one another
+  if (/-?\d+ [-+/*] $/.test(display.textContent)) {
+    let string = display.textContent.split('');
+    string[string.length - 2] = event.target.textContent;
+    display.textContent = string.join('');
+  }
+  if (/^$/.test(display.textContent) && event.target.textContent == '-') {
+    display.textContent += event.target.textContent;
+  } else if (/^-?\d+$/.test(display.textContent)) {
+    display.textContent += ` ${event.target.textContent} `;
+  } else if (/^-?\d+ [-+/*] \d+$/.test(display.textContent)) {
+    display.textContent = `${operate()} ${event.target.textContent} `;
   }
 }
 
@@ -71,14 +77,14 @@ let buttons = document.querySelectorAll('button');
 let display = document.querySelector('#display');
 
 buttons.forEach((button) => {
-  if (isNumber(button.textContent)) {
+  if (/[0-9]/.test(button.textContent)) {
     button.addEventListener('click', addNumber);
   } else if (button.textContent === '=') {
     button.addEventListener('click', equals);
   } else if (button.textContent === 'Clear') {
     button.addEventListener('click', clear);
-  } else if (isOperator(button.textContent)) {
-    // button.addEventListener('click', operatorButton);
+  } else if (/[-+/*]/.test(button.textContent)) {
+    button.addEventListener('click', addOperator);
   } else if (button.textContent === '.') {
     // button.addEventListener('click', dotButton);
   }
